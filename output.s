10a48fd33a094359844be241d5bf46f32176465f
deadlightreal <richardfabianmain@gmail.com>
2024-08-30 15:09:59 +0200

    fixed comparing

--- output.s
+++ output.s
@@ -2,41 +2,49 @@
 .align 2
 _start:
 
-    adr X10, f_test_end
-    adrp X11, fn_end@PAGE
-    add X11, X11, fn_end@PAGEOFF
-    str X10, [X11]
-    bl f_test
+    mov W1, #10
 
-f_test_end:
+    mov W2, #5
+
+    cmp W1, W2
+
+    b.eq equal_0
+
+    b.ne not_equal_0
+
+    bl continue_0
+equal_0:
+    mov X0, #1
+    adrp X1, print_string_0@PAGE
+    add X1, X1, print_string_0@PAGEOFF
+    mov X2, 6
+    mov X16, #4
+    svc #0x80
+
+
+
+    bl continue_0
+not_equal_0:
     mov X0, #1
     adrp X1, print_string_1@PAGE
     add X1, X1, print_string_1@PAGEOFF
-    mov X2, 22
+    mov X2, 10
     mov X16, #4
     svc #0x80
+
+
+
+    bl continue_0
+
+
+continue_0:
 
     mov X0, #0
     mov X16, #1
     svc #0x80
 
-f_test:
-        mov X0, #1
-    adrp X1, print_string_0@PAGE
-    add X1, X1, print_string_0@PAGEOFF
-    mov X2, 17
-    mov X16, #4
-    svc #0x80
-
-
-    
-    adrp X10, fn_end@PAGE
-    add X10, X10, fn_end@PAGEOFF
-    ldr X11, [X10]
-    br X11
-
 .data
 new_line: .ascii "\n"
 fn_end: .quad _start
-print_string_0: .ascii "in test function\n"
-print_string_1: .ascii "outside test function\n"
+print_string_0: .ascii "equal\n"
+print_string_1: .ascii "not equal\n"
