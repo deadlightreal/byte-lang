7f2bb3f7c63090ed92d1e99efc5d335ac2089355
deadlightreal <139078351+deadlightreal@users.noreply.github.com>
2024-08-31 15:12:15 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-as -o output.o test.s && ld -macos_version_min 11.0.0 -o output output.o -lSystem -syslibroot `xcrun -sdk macosx --show-sdk-path` -e _start -arch arm64
+as -o output.o output.s && ld -macos_version_min 11.0.0 -o output output.o -lSystem -syslibroot `xcrun -sdk macosx --show-sdk-path` -e _start -arch arm64
